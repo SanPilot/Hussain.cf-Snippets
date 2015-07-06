36c43162878d0641ccda182bb7a3e9c5003df57c
Hussain Khalil <contact@hussain.cf>
2015-07-06 16:51:08 -0700

    Added saved date

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if(is_file("files/".$_GET['f']) && isset($_GET['f']) && $_GET['f'] != "") {
 	$filename = htmlspecialchars($_GET['f']);
-	$modate = filemtime("files/".$_GET['f']);
+	$modate = date("F j, Y", filemtime("files/".$_GET['f'])) . " at " . date("g:i a", filemtime("files/".$_GET['f']));
 } elseif (isset($_GET['f']) && $_GET['f'] != "") {
 	include "../error/index.php";
 	exit();
@@ -42,13 +42,18 @@ if(!$new) {
 				color: #ebebeb;
 				display: inline-block;
 			}
+			#headersmall {
+				font-size: 20px;
+				color: #aaaaaa;
+				<?php if($new) echo "display: none;"; ?>
+			}
 			<?php include "../scripts/php/clippings/fonts/PermianSerif.php" ?>
 		</style>
 		<?php include "../scripts/php/clippings/header.php" ?>
 	</head>
 	<body>
 		<div id="header">
-			<span id="headerlarge"><?=$filename ?></span>
+			<span id="headerlarge"><?=$filename ?></span><span id="headersmall">&nbsp;&#x2013;&nbsp; saved <?=$modate ?></span>
 		</div>
 		<div id="area" placeholder="// Paste your snippet here!" class="hljs" contenteditable="<?php if($new) {echo "true";} else {echo "false";} ?>">
 			var google = go.ecex();
