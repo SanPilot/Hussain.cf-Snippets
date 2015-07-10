0a7776f1fe0c40f9d85adaf84d43ef5635a4b76f
Hussain Khalil <contact@hussain.cf>
2015-07-10 11:40:40 -0700

    Lots of styling and line numbers

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 if(is_file("files/".$_GET['f']) && isset($_GET['f']) && $_GET['f'] != "") {
 	$filename = htmlspecialchars($_GET['f']);
 	$modate = date("F j, Y", filemtime("files/".$_GET['f'])) . " at " . date("g:i a", filemtime("files/".$_GET['f']));
+	$lang = preg_match("/.*\.(\w+)$/", $_GET['f'], $res);
+	$lang = $res[1];
+	$file = "files/".$_GET['f'];
+	$linecount = 0;
+	$handle = fopen($file, "r");
+	while(!feof($handle)){
+		$line = fgets($handle);
+		$linecount++;
+	}
+	fclose($handle);
 } elseif (isset($_GET['f']) && $_GET['f'] != "") {
 	header("status: 404 Not Found");
 	include "../error/index.php";
@@ -28,22 +38,30 @@ if(!$new) {
 }
 			?>
 		</title>
-		<link rel="stylesheet" href="highlightjs/styles/hybrid.css">
+		<link rel="stylesheet" href="highlightjs/styles/obsidian.css">
 		<style type="text/css">
-			body {
+			body, html {
 				margin: 0px;
-				background-color: #1d1f21;
-				padding: 10px;
+				padding: 0px;
+				height: 100%;
+				background-color: #282b2e;
+			}
+			#wrapper {
+				height: 100%;
 			}
 			#area {
 				outline: none;
 				visibility: hidden;
 			}
+
 			#header {
 				display: flex;
 				align-items: center;
 				font-family: Permian Serif;
-				margin-left: 8px;
+				padding: 18px 0 18px 18px;
+				background-color: #1d1f21;
+				position: fixed;
+				width: 100%;
 			}
 			#headerlarge {
 				font-size: 25px;
@@ -54,32 +72,61 @@ if(!$new) {
 				font-size: 20px;
 				color: #aaaaaa;
 			}
+			#areacontainer > pre {
+				margin: 0px;
+				padding-left: 18px;
+				height: 100%;
+				padding-top: 79px;
+				padding-bottom: 10px;
+				font-size: 13px;
+			}
+			#line-numbers {
+				height: 100%;
+				float: left;
+				background-color: #3e4348;
+				color: #ffffff;
+				padding: 79px 18px 10px 18px;
+				font-size: 13px;
+				text-align: center;
+				font-family: monospace;
+			}
 			<?php include "../scripts/php/clippings/fonts/PermianSerif.php" ?>
 		</style>
 		<?php include "../scripts/php/clippings/header.php" ?>
 	</head>
 	<body>
-		<div id="header">
-			<span id="headerlarge"><?=$filename ?></span><span id="headersmall">&nbsp;&#x2013;&nbsp; <span id="ctext">loading...</span></span>
-		</div>
-		<div id="area" class="hljs" contenteditable="<?php if($new) {echo "true";} else {echo "false";} ?>">
-			<?=$fcontent ?>
+		<div id="wrapper">
+			<div id="header">
+				<span id="headerlarge"><?=$filename ?></span><span id="headersmall">&nbsp;&#x2013;&nbsp; <span id="ctext">loading...</span></span>
+			</div>
+			<div id="areacontainer">
+				<div id="line-numbers"></div>
+				<pre id="area" class="hljs js" contenteditable="<?php if($new) {echo "true";} else {echo "false";} ?>"><?=$fcontent ?></pre>
+			</div>
 		</div>
 		<script type="text/javascript" src="highlightjs/highlight.pack.js"></script>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script type="text/javascript">
 			var area = document.getElementById("area");
 			var newf = <?php if($new) {echo "true";} else {echo "false";} ?>;
-				window.onload = function() {
-					hljs.configure({useBR: true});
-					hljs.highlightBlock(area);
-					if(newf) {
-						document.getElementById("headersmall").innerHTML = "";
-					} else {
-						document.getElementById("ctext").innerHTML = "saved <?=$modate ?>";
+			if(!newf) {
+				var lines = <?=$linecount; ?>;
+				}
+			var i = 1;
+			window.onload = function() {
+				hljs.configure({useBR: true});
+				hljs.highlightBlock(area);
+				if(newf) {
+					document.getElementById("headersmall").innerHTML = "";
+				} else {
+					document.getElementById("ctext").innerHTML = "saved <?=$modate ?>";
+					while(i <= lines) {
+						document.getElementById("line-numbers").innerHTML += i+"<br>\n";
+						i++;
 					}
-					area.style.visibility = "visible";
-				};
+				}
+				area.style.visibility = "visible";
+			};
 			area.onkeyup = function() {
 				hljs.highlightBlock(area);
 			};
