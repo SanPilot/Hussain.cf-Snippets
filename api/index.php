1727d81226103fabc5bef437cce8c6af9feb706e
Hussain Khalil <hussainpilot05@gmail.com>
2015-07-29 14:43:00 -0700

    Regex fix... ?

--- api/index.php
+++ api/index.php
@@ -54,7 +54,7 @@ if(isset($_POST['publish'], $_POST['content']) && $_POST['publish'] != "" && $_P
 		"status" => false,
 		"error" => "There was an error publishing the snippet"
 	);
-	if(!preg_match("/[^/?*:;{}\\]+/", $_POST['publish'])) {
+	if(!preg_match("[^/?*:;{}\\]+", $_POST['publish'])) {
 		if(strlen($filename) <= 40) {
 			if(strlen($filename) > 0) {
 				if(is_dir("../".$filename) || is_file("../".$filename) || is_file("../files/".$filename)) {
