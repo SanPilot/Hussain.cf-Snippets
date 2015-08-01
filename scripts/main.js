8f0722b8a4c0a60f3d1c356a5baf19d8addd14c1
Hussain Khalil <hussainpilot05@gmail.com>
2015-07-31 19:19:31 -0700

    ensure changestyle works correctly

--- scripts/main.js
+++ scripts/main.js
@@ -193,7 +193,7 @@ var changeStyle = function(newstyle) {
 	}, 1);
 	setTimeout(function() {
 		clearInterval(changestyleinterval);
-	}, 500);
+	}, 600);
 };
 var getParameterByName = function(name) {
 	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
